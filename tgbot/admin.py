0601975fbf64c603cb40ec8330f97aeb716ecef4
Max Kutovoy <maxkutovoy@yandex.ru>
2022-03-16 20:08:44 +0600

    Add models to admin panel

--- tgbot/admin.py
+++ tgbot/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import Dish, Product, Subscribe, User, Preference, Allergy
 # Register your models here.
+
+admin.site.register(Dish)
+admin.site.register(Product)
+admin.site.register(Subscribe)
+admin.site.register(User)
+admin.site.register(Preference)
+admin.site.register(Allergy)
