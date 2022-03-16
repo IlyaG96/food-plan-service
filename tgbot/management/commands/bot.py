00cc6cae9d94525a81738b7ce1a718f10118e5ee
IlyaG96 <ilyag_96@icloud.com>
2022-03-16 12:22:42 +1100

    fix back buttons

--- tgbot/management/commands/bot.py
+++ tgbot/management/commands/bot.py
@@ -1,15 +1,12 @@
-from textwrap import dedent
 from enum import Enum
 from django.conf import settings
 from django.core.management.base import BaseCommand
 from telegram import (
     ReplyKeyboardMarkup,
-    ReplyMarkup,
     ReplyKeyboardRemove,
     InlineKeyboardMarkup,
     InlineKeyboardButton,
     KeyboardButton,
-    Update,
     message
 )
 from telegram.ext import (CallbackContext, CallbackQueryHandler, CommandHandler, ConversationHandler,
@@ -38,12 +35,15 @@ def start(update, context):
 
 
 def get_user_phonenumber(update, context):
-    context.user_data['name'] = update.message.text
+
+    if update.message.text != 'Назад ⬅':
+        context.user_data['name'] = update.message.text
+
     phone_request_button = KeyboardButton('Передать контакт', request_contact=True)
     update.message.reply_text(
         'Мне понадобится номер телефона. Можно отправить свой номер из профиля или ввести вручную',
         reply_markup=ReplyKeyboardMarkup(
-            [[phone_request_button]],
+            [[phone_request_button, 'Назад ⬅']],
             resize_keyboard=True,
             input_field_placeholder='8-999-999-9999',
         ),
@@ -61,15 +61,15 @@ def get_portion_size(update, context):
             phone_number = update.message.text
         context.user_data['phonenumber'] = phone_number
 
-        # TODO replace from db
-        keyboard = [[str(num) for num in range(1, 11)], ['Назад ⬅']]
-        update.message.reply_text(
-            'На какое количество человек рассчитывать блюдо?',
-            reply_markup=ReplyKeyboardMarkup(keyboard=keyboard,
-                                             resize_keyboard=True,
-                                             ),
-        )
-        return BotStates.GET_PORTIONS_SIZE
+    # TODO replace from db
+    keyboard = [[str(num) for num in range(1, 11)], ['Назад ⬅']]
+    update.message.reply_text(
+        'На какое количество человек рассчитывать блюдо?',
+        reply_markup=ReplyKeyboardMarkup(keyboard=keyboard,
+                                         resize_keyboard=True,
+                                         ),
+    )
+    return BotStates.GET_PORTIONS_SIZE
 
 
 def get_preferences(update, context):
@@ -77,21 +77,21 @@ def get_preferences(update, context):
         portion_size = update.message.text
         context.user_data['portion_size'] = portion_size
 
-        keyboard = [['Мясоед',
-                     'Предпочитаю рыбов',
-                     'Убежденный веган',
-                     'Всего и побольше',
-                     'Низкокалорийная диета',
-                     ], ['Назад ⬅']]
-        # TODO replace from db
-        # TODO если какая-то низкокалорийная диета, то можно добавить выбор блюд по калориям. Но это уже к фичам,
-        #  наверное
-        update.message.reply_text(
-            'Есть какие-то предпочтения по диете?',
-            reply_markup=ReplyKeyboardMarkup(keyboard=keyboard,
-                                             resize_keyboard=True,
-                                             ),
-        )
+    keyboard = [['Мясоед',
+                 'Предпочитаю рыбов',
+                 'Убежденный веган',
+                 'Всего и побольше',
+                 'Низкокалорийная диета',
+                 ], ['Назад ⬅']]
+    # TODO replace from db
+    # TODO если какая-то низкокалорийная диета, то можно добавить выбор блюд по калориям. Но это уже к фичам,
+    #  наверное
+    update.message.reply_text(
+        'Есть какие-то предпочтения по диете?',
+        reply_markup=ReplyKeyboardMarkup(keyboard=keyboard,
+                                         resize_keyboard=True,
+                                         ),
+    )
 
     return BotStates.GET_PREFERENCES
 
@@ -101,13 +101,13 @@ def get_portions_quantity(update, context):
         preferences = update.message.text
         context.user_data['preferences'] = preferences
         # TODO replace from db
-        keyboard = [[str(num) for num in range(1, 11)], ['Назад ⬅']]
-        update.message.reply_text(
-            'Сколько порций потребуется?',
-            reply_markup=ReplyKeyboardMarkup(keyboard=keyboard,
-                                             resize_keyboard=True,
-                                             ),
-        )
+    keyboard = [[str(num) for num in range(1, 11)], ['Назад ⬅']]
+    update.message.reply_text(
+        'Сколько порций потребуется?',
+        reply_markup=ReplyKeyboardMarkup(keyboard=keyboard,
+                                         resize_keyboard=True,
+                                         ),
+    )
 
     return BotStates.GET_PORTIONS_QUANTITY
 
@@ -117,13 +117,13 @@ def get_subscription_length(update, context):
         portions_quantity = update.message.text
         context.user_data['portions_quantity'] = portions_quantity
         # TODO replace from db
-        keyboard = [[str(num) for num in range(1, 13)], ['Назад ⬅']]
-        update.message.reply_text(
-            'На сколько месяцев Вы хотите оформить подписку?',
-            reply_markup=ReplyKeyboardMarkup(keyboard=keyboard,
-                                             resize_keyboard=True,
-                                             ),
-        )
+    keyboard = [[str(num) for num in range(1, 13)], ['Назад ⬅']]
+    update.message.reply_text(
+        'На сколько месяцев Вы хотите оформить подписку?',
+        reply_markup=ReplyKeyboardMarkup(keyboard=keyboard,
+                                         resize_keyboard=True,
+                                         ),
+    )
     return BotStates.GET_SUBSCRIPTION_LENGTH
 
 
@@ -150,7 +150,7 @@ def main():
         states={
 
             BotStates.GET_USERNAME: [
-                MessageHandler(Filters.regex(r'[а-яА-Я]{2,20}( )[а-яА-Я]{2,20}$'),
+                MessageHandler(Filters.regex(r'[а-яА-Яa-zA-Z]{2,20}( )[а-яА-Яa-zA-Z]{2,20}$'),
                                get_user_phonenumber),
                 MessageHandler(Filters.text, start)
             ],
@@ -158,21 +158,25 @@ def main():
             BotStates.GET_PHONENUMBER: [
                 MessageHandler(Filters.regex(r'^\+?\d{1,3}?( |-)?\d{3}( |-)?\d{3}( |-)?\d{2}( |-)?\d{2}$'),
                                get_portion_size),
+                MessageHandler(Filters.regex(r'^Назад ⬅$'), start),
                 MessageHandler(Filters.text, get_user_phonenumber)
             ],
 
             BotStates.GET_PORTIONS_SIZE: [
                 MessageHandler(Filters.regex(r'[0-9]{1,2}$'), get_preferences),
+                MessageHandler(Filters.regex(r'^Назад ⬅$'), get_user_phonenumber),
                 MessageHandler(Filters.text, get_portion_size)
             ],
 
             BotStates.GET_PREFERENCES: [
                 MessageHandler(Filters.regex(r'[а-яА-Я]{2,15}$'), get_subscription_length),
+                MessageHandler(Filters.regex(r'^Назад ⬅$'), get_portion_size),
                 MessageHandler(Filters.text, get_preferences)
             ],
 
             BotStates.GET_SUBSCRIPTION_LENGTH: [
                 MessageHandler(Filters.regex(r'[0-9]{1,2}$'), check_order),
+                MessageHandler(Filters.regex(r'^Назад ⬅$'), get_preferences),
                 MessageHandler(Filters.text, get_subscription_length)
             ],
 
