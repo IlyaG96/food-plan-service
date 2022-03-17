c1f7cc03cbfdff8f6f53f0f2bb0a19c859ac5adc
IlyaG96 <ilyag_96@icloud.com>
2022-03-17 18:00:47 +1100

    add favourite and unloved dishes to user

--- tgbot/models.py
+++ tgbot/models.py
@@ -23,20 +23,6 @@ class Allergy(models.Model):
         verbose_name_plural = 'Аллергии'
 
 
-class User(models.Model):
-    first_name = models.CharField('Имя', max_length=250)
-    last_name = models.CharField('Фамилия', max_length=250, blank=True)
-    chat_id = models.IntegerField('Chat id', unique=True)
-    phone = models.CharField('Номер телефона', max_length=20, blank=True)
-
-    def __str__(self):
-        return f'ID: {self.chat_id} имя: {self.first_name} телефон: {self.phone}'
-
-    class Meta:
-        verbose_name = 'Подписчик'
-        verbose_name_plural = 'Подписчики'
-
-
 class Product(models.Model):
     title = models.CharField('Название продукта', max_length=200)
 
@@ -86,6 +72,35 @@ class Dish(models.Model):
     class Meta:
         verbose_name = 'Блюдо'
         verbose_name_plural = 'Блюда'
+
+
+class User(models.Model):
+    first_name = models.CharField('Имя', max_length=250)
+    last_name = models.CharField('Фамилия', max_length=250, blank=True)
+    chat_id = models.IntegerField('Chat id', unique=True)
+    phone = models.CharField('Номер телефона', max_length=20, blank=True)
+    favorite_dishes = models.ForeignKey(Dish,
+                                        verbose_name='Любимые блюда',
+                                        on_delete=models.CASCADE,
+                                        related_name='favourite',
+                                        null=True,
+                                        blank=True,
+                                        default=None)
+
+    unloved_dishes = models.ForeignKey(Dish,
+                                       verbose_name='Нелюбимые блюда',
+                                       on_delete=models.CASCADE,
+                                       related_name='unloved',
+                                       null=True,
+                                       blank=True,
+                                       default=None)
+
+    def __str__(self):
+        return f'ID: {self.chat_id} имя: {self.first_name} телефон: {self.phone}'
+
+    class Meta:
+        verbose_name = 'Подписчик'
+        verbose_name_plural = 'Подписчики'
 
 
 class Subscribe(models.Model):
