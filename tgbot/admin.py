0aa1921b80afc5da5b90e26863f8aa8bec0a80c7
IlyaG96 <ilyag_96@icloud.com>
2022-03-17 13:56:38 +1100

    show user's subscriptions

--- tgbot/admin.py
+++ tgbot/admin.py
@@ -5,7 +5,24 @@ from .models import Dish, Product, Subscribe, User, Preference, Allergy
 
 admin.site.register(Dish)
 admin.site.register(Product)
-admin.site.register(Subscribe)
-admin.site.register(User)
 admin.site.register(Preference)
 admin.site.register(Allergy)
+
+
+class SubscriptionAdmin(admin.StackedInline):
+    model = Subscribe
+
+
+@admin.register(User)
+class User(admin.ModelAdmin):
+    inlines = [
+        SubscriptionAdmin
+    ]
+
+    class Meta:
+        model = User
+
+
+@admin.register(Subscribe)
+class Image(admin.ModelAdmin):
+    raw_id_fields = ('subscriber', 'preference')
