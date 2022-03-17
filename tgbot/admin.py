f1829b33cd6d33b9c6293affc755157700b81c50
IlyaG96 <ilyag_96@icloud.com>
2022-03-17 14:15:53 +1100

    fix model name

--- tgbot/admin.py
+++ tgbot/admin.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
 
 from .models import Dish, Product, Subscribe, User, Preference, Allergy
-# Register your models here.
 
-admin.site.register(Dish)
 admin.site.register(Product)
 admin.site.register(Preference)
 admin.site.register(Allergy)
+admin.site.register(Dish)
 
 
 class SubscriptionAdmin(admin.StackedInline):
@@ -24,5 +23,6 @@ class User(admin.ModelAdmin):
 
 
 @admin.register(Subscribe)
-class Image(admin.ModelAdmin):
-    raw_id_fields = ('subscriber', 'preference')
+class Subscribe(admin.ModelAdmin):
+    raw_id_fields = ('subscriber', 'preference', 'allergy')
+
